30cd11c278aabb249a2b4db0d48e11499d1cd4ba
ngactive99 <ngactive99@gmail.com>
2022-04-28 14:57:22 +0530

    exception handler

--- src/main/java/com/demo/spring/model/Employee.java
+++ src/main/java/com/demo/spring/model/Employee.java
@@ -1,6 +1,7 @@
 package com.demo.spring.model;
 
 import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- src/main/java/com/demo/spring/repository/EmployeeRepository.java
+++ src/main/java/com/demo/spring/repository/EmployeeRepository.java
@@ -1,5 +1,9 @@
 package com.demo.spring.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.demo.spring.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+	
 
 }
